42464da745771aa419566e1deb718c1247eaa0ca
petre <petre@PETRE-LAPTOP>
2023-06-05 21:35:51 +0300

    Add project files.

---
+++ Videogames/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Videogames.Models
+{
+    public class Game
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
